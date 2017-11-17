e0bf63ca12da36c23bf9462ea5ed5f69b49c1412
Lisette <lisettem50288@students.laalliance.org>
2017-11-17 17:21:49 +0000

    Done.

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var countries = [
+    "Chad",
+    "Cuba",
+    "Greenland",
+    "Iraq",
+    "Mali",
+    "Oman",
+    "India"
+    ]
+    
+console.log(countries)
+    
+    countries.push("Fiji")
+    countries.push("Laos")
+    countries.shift("Peru")
+    countries.reverse(array)
+    Function deleteSixthItem(array){
+        array.splice
+    }
